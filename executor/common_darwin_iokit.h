e9012c747f9c300f7d78604fa486a925328ffa9c
jmpews <jmpews@gmail.com>
2021-12-02 15:46:25 +0800

    sys/darwin: add driver interface description

--- executor/common_darwin_iokit.h
+++ executor/common_darwin_iokit.h
@@ -1,17 +1,38 @@
-#include <stdio.h>
-#include <stddef.h>
 #include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
 
-#include <servers/bootstrap.h>
+#include <IOKit/IOKitLib.h>
 #include <mach/mach.h>
 #include <mach/message.h>
+#include <servers/bootstrap.h>
 #include <stdlib.h>
 #include <sys/sysctl.h>
 #include <unistd.h>
-#include <IOKit/IOKitLib.h>
 
-kern_return_t syz_io_connect_call_method(mach_port_t connection, uint32_t selector, const uint64_t *input, uint32_t inputCnt, const void *inputStruct, size_t inputStructCnt, uint64_t *output, uint32_t *outputCnt, void *outputStruct, size_t *outputStructCnt) {
-  kern_return_t ret = 0;
-  ret = IOConnectCallMethod(connection, selector, input, inputCnt, inputStruct, inputStructCnt, output, outputCnt, outputStruct, outputStructCnt);
-  return ret;
+mach_port_t syz_open_service(const char* name, uint32_t type)
+{
+	io_service_t gpu = IO_OBJECT_NULL;
+	io_connect_t connect = IO_OBJECT_NULL;
+	IOReturn ioret;
+
+	gpu = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("IOGPU"));
+	if (gpu == IO_OBJECT_NULL) {
+		fprintf(stderr, "client: unable to find IOGPU service\n");
+		exit(1);
+	}
+
+	uint32_t open_type = 5;
+	ioret = IOServiceOpen(gpu, mach_task_self(), open_type, &connect);
+	if (ioret != kIOReturnSuccess) {
+		fprintf(stderr, "client: unable to open user client: 0x%x\n", ioret);
+		exit(1);
+	}
+}
+
+kern_return_t syz_io_connect_call_method(mach_port_t connection, uint32_t selector, const uint64_t* input, uint32_t inputCnt, const void* inputStruct, size_t inputStructCnt, uint64_t* output, uint32_t* outputCnt, void* outputStruct, size_t* outputStructCnt)
+{
+	kern_return_t ret = 0;
+	ret = IOConnectCallMethod(connection, selector, input, inputCnt, inputStruct, inputStructCnt, output, outputCnt, outputStruct, outputStructCnt);
+	return ret;
 }
